cb111db7e0a421055a6135aef2c9caf7ad5581aa
ArvidHast <hasta@student.chalmers.se>
2022-01-01 21:48:48 +0100

    Clean & start mining reward

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
 import hashlib
 import math
+import random
+import json
 
-# TODO add possibility of having more than a 16th of a 2 exponential in difficulty
-def solveHash(difficulty, key):
-    n = 1
-    zeros = math.log(difficulty, 2)
+
+TRANSACTIONS_PER_BLOCK = 10
+BLOCK_DIFFICULTY = 2 ** 4  # block difficulty is expressed linearly
+BLOCK_REWARD = 100
+
+thisWallet = random.randbytes(16).hex()
+print("this wallet: ", thisWallet)
+
+
+def solveHash(blockData):
+    n = random.randint(
+        0, 1000000000
+    )  # each miner randomizes start point, making expected value linear to hasing power
+    zeros = math.log(BLOCK_DIFFICULTY, 2)
     wholeZeros = math.floor(zeros - zeros % 1)
     extraRequired = hex(round(0xF * (zeros % 1)))
 
-    res = hashlib.sha384((key + str(n)).encode("utf-8")).hexdigest()
-
+    res = hashlib.sha384((blockData + str(n)).encode("utf-8")).hexdigest()
     remainder = 0xF
-
     while res[:wholeZeros] != "0" * wholeZeros or remainder < extraRequired:
-        res = hashlib.sha384((key + str(n)).encode("utf-8")).hexdigest()
+        res = hashlib.sha384((blockData + str(n)).encode("utf-8")).hexdigest()
         n += 1
         remainder = hex(int(res[wholeZeros : wholeZeros + 1], 16))
     return n, res
 
 
-difficulty = 5
-key = "Hello ladies and gentlemen, this is the start of something very stupid :)"
-blockdata = "This is some block data that is being signed, prevBlock: " + key
+key = "0"
+blockdata = "{ blockNumber: 0, prevBlock: " + str(key) + "}"
 
 blocks = []
 
-# blocks
-for i in range(100):
-    iterations, solution = solveHash(difficulty, str(blockdata))
-    print(i, iterations)
+ledger = open("ledger.json")
 
+# blocks
+for i in range(2):
+    nonce, solution = solveHash(str(blockdata))
     key = solution
 
-    blockdata = "This is some block data that is being signed, prevBlock: " + str(key)
+    transactions = [{"from": 0, "to": thisWallet, "amount": BLOCK_REWARD}]
+
+    blockdata = {
+        "blockNumber": i,
+        "hash": key,
+        "prevBlockHash": blocks[i - 1]["hash"] if i else "0",
+        "nonce": nonce,
+        "transactions": transactions,
+    }
+
+    print(blockdata)
     blocks.append(blockdata)
 
 print(blocks)
